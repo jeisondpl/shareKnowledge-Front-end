27d5915bff81b2a648b52f14c8a393d9e3ca97f9
jeison diaz palmera <jadiazp@indracompany.com>
2022-08-05 10:34:21 -0500

    config deploy 4

--- config/apollo.tsx
+++ config/apollo.tsx
@@ -3,8 +3,9 @@ import { setContext } from '@apollo/client/link/context'
 import * as fetch from 'cross-fetch'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3000/api/graphql',
-  // uri: 'https://share-knowledge-back-end-indra-deploy-jeisondiaz.vercel.app/',
+  // uri: 'http://localhost:3000/api/graphql',
+  // uri: 'https://share-knowledge-front-end.vercel.app/api/graphql',
+  uri: 'https://share-knowledge-front-end-deploy.vercel.app/api/graphql',
 
   fetch: fetch as any,
 })
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -28,18 +28,24 @@ const apolloServer = new ApolloServer({
       }
     }
   },
+  introspection: true,
+
 })
 
 const startServer = apolloServer.start()
 
-export default cors(async function handler(req: any, res: any) {
+
+
+
+
+export default cors(async (req: any, res: any) => {
 
   if (req.method === 'OPTIONS') {
     res.end()
     return false
   }
   await startServer;
-  await apolloServer.createHandler({ path: '/api/graphql', })(req, res);
+  await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
 });
 
 
